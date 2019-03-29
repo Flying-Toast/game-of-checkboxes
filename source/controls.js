f9579b6d2aca3b776edd7cef4c5bbbd734d919cb
Flying-Toast <38232168+Flying-Toast@users.noreply.github.com>
2019-03-29 19:01:52 -0400

    button to evolve 1 gen

--- source/controls.js
+++ source/controls.js
@@ -1,3 +1,4 @@
 let playPauseButton = document.querySelector("#togglePause");
 
 playPauseButton.addEventListener("click", playPause);
+document.querySelector("#singleEvolve").addEventListener("click", tick);
